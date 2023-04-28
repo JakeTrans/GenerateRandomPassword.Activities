25985bfb534a9a11b1d0b6e83bed98e7d79b2d66
Jake Trans <Jake_Damner@yahoo.co.uk>
2023-04-28 20:04:55 +0100

    Secure String option added

--- UiPathTeam.GenerateRandomPassword.Activities/GenerateRandomPassword.cs
+++ UiPathTeam.GenerateRandomPassword.Activities/GenerateRandomPassword.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Activities;
+﻿using System.Activities;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Security;
+using System;
 
 namespace UiPathTeam.GenerateRandomPassword.Activities
+
 {
     [DisplayName("Generate Random Password")]
-    public class GenerateRandomPassword: CodeActivity
+    public class GenerateRandomPassword : CodeActivity
+
     {
         [Category("Input")]
         [DisplayName("Minimum password length")]
@@ -23,12 +26,12 @@ namespace UiPathTeam.GenerateRandomPassword.Activities
         [Category("Input")]
         [DisplayName("Required digits")]
         [Description("Maximum allowed value is 1000 characters")]
-        public InArgument<Int32> RequiredDigits{ get; set; }
+        public InArgument<Int32> RequiredDigits { get; set; }
 
         [Category("Input")]
         [DisplayName("Required uppercase letters")]
         [Description("Maximum allowed value is 1000 characters")]
-        public InArgument<Int32> RequiredUpperCaseLetters{ get; set; }
+        public InArgument<Int32> RequiredUpperCaseLetters { get; set; }
 
         [Category("Input")]
         [DisplayName("Required lowercase letters")]
@@ -49,20 +52,33 @@ namespace UiPathTeam.GenerateRandomPassword.Activities
         [DisplayName("Password")]
         public OutArgument<string> Password { get; set; }
 
+        [Category("Output")]
+        [DisplayName("SecurePassword")]
+        public OutArgument<SecureString> SecurePassword { get; set; }
+
         public const int CapSize = 1000;
 
         private static string LowerCaseLetters = "abcdefgijkmnopqrstwxyz";
+
         private static string UpperCaseLetters = "ABCDEFGHJKLMNPQRSTWXYZ";
+
         private static string Digits = "0123456789";
+
         private static string SpecialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
         protected override void Execute(CodeActivityContext context)
+
         {
             var minLength = MinLength.Get(context);
+
             var maxLength = MaxLength.Get(context);
+
             var requiredDigits = RequiredDigits.Get(context);
+
             var requiredLowerCaseLetters = RequiredLowerCaseLetters.Get(context);
+
             var requiredUpperCaseLetters = RequiredUpperCaseLetters.Get(context);
+
             var requiredNonAlphaNumericChars = RequiredSpecialChars.Get(context);
 
             SpecialChars = string.IsNullOrEmpty(AllowedSpecialChars.Get(context)) ? SpecialChars : AllowedSpecialChars.Get(context);
@@ -70,69 +86,98 @@ namespace UiPathTeam.GenerateRandomPassword.Activities
             var allAvailableChars = LowerCaseLetters + UpperCaseLetters + Digits + SpecialChars;
 
             if (new[] { minLength, maxLength, requiredDigits,
+
                 requiredLowerCaseLetters,
+
                 requiredUpperCaseLetters,
+
                 requiredNonAlphaNumericChars }.Any(item => item > CapSize))
+
             {
                 throw new ArgumentException("All character requirements have to be lower or equal to " + CapSize);
             }
 
             if (minLength > maxLength)
+
             {
                 throw new ArgumentException("Minimum password length cannot be greater than maximum password length");
             }
 
             if (maxLength < requiredDigits + requiredLowerCaseLetters + requiredUpperCaseLetters + requiredNonAlphaNumericChars)
+
             {
                 throw new ArgumentException("Maximum length is lower than total required length");
             }
 
             var randomBytes = new Byte[4];
+
             var rngCrypto = new RNGCryptoServiceProvider();
+
             rngCrypto.GetBytes(randomBytes);
 
             var randomGenerator = new Random(BitConverter.ToInt32(randomBytes, 0));
 
             var passwordLength = randomGenerator.Next(minLength, maxLength + 1);
+
             var selectedChars = new List<Char>();
 
             for (int i = 0; i < requiredDigits; i++)
+
             {
                 selectedChars.Add(Digits.ToCharArray()[randomGenerator.Next(0, Digits.Length)]);
             }
 
             for (int i = 0; i < requiredLowerCaseLetters; i++)
+
             {
                 selectedChars.Add(LowerCaseLetters.ToCharArray()[randomGenerator.Next(0, LowerCaseLetters.Length)]);
             }
 
             for (int i = 0; i < requiredUpperCaseLetters; i++)
+
             {
                 selectedChars.Add(UpperCaseLetters.ToCharArray()[randomGenerator.Next(0, UpperCaseLetters.Length)]);
             }
 
             for (int i = 0; i < requiredNonAlphaNumericChars; i++)
+
             {
                 selectedChars.Add(SpecialChars.ToCharArray()[randomGenerator.Next(0, SpecialChars.Length)]);
             }
 
             var remainingChars = passwordLength - selectedChars.Count;
+
             for (int i = 0; i < remainingChars; i++)
+
             {
                 selectedChars.Add(allAvailableChars.ToCharArray()[randomGenerator.Next(0, allAvailableChars.Length)]);
             }
 
             var passwordCharArray = new Char[passwordLength];
+
             var emptyLocations = Enumerable.Range(0, passwordLength).ToList();
 
             foreach (var selectedChar in selectedChars)
+
             {
                 var emptyLocationIndex = randomGenerator.Next(0, emptyLocations.Count);
+
                 passwordCharArray[emptyLocations[emptyLocationIndex]] = selectedChar;
+
                 emptyLocations.RemoveAt(emptyLocationIndex);
             }
 
             Password.Set(context, string.Join("", passwordCharArray));
+
+            SecureString sec = new SecureString();
+
+            foreach (char c in passwordCharArray)
+
+            {
+                sec.AppendChar(c);
+            }
+
+            SecurePassword.Set(context, sec);
         }
     }
 }
